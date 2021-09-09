6651f45c17fbbbe455eb30cc4681d5ab97aa865b
Kruthic Vignesh M <80332221+Kruthic-Vignesh@users.noreply.github.com>
2021-09-09 14:42:25 +0530

    Update test_gen.cpp

--- test_gen.cpp
+++ test_gen.cpp
@@ -14,43 +14,43 @@ int randf(int n)
 
 int main()
 {
-	freopen("myoutfile2.txt", "w", stdout);
+    freopen("myoutfile2.txt", "w", stdout);
     ll t = 4;
     cout<<t<<endl;
     for(ll z = 0; z < t; z++)
     {
-	    int n = rand()%100;
-	    cout<<n<<endl;
-	    int men[n][n], women[n][n];
-	    
-	    int posm[n][n], posw[n][n];
-	    for(int i = 0; i < n; i++)
-	    {
-	        for(int j = 0; j < n; j++)
-	        {
-	            men[i][j] = j;
-	            women[i][j] = j;
-	        }
-	    }
-	    for(int i = 0; i < n; i++)
-	    {
-	        random_shuffle(men[i], men[i]+n, randf);        
-	        for(int j = 0; j < n; j++) 
-	        {
-	            cout<<men[i][j]<<" ";  
-	        }
-	        cout<<endl;
-	    }
-	    for(int i = 0; i < n; i++) 
-	    {
-	        random_shuffle(women[i], women[i]+n, randf);
-	        for(int j = 0; j < n; j++) 
-	        {
-	            cout<<women[i][j]<<" "; 
-	        }
-	        cout<<endl;
-	    }
-	}
-	fclose(stdout);
+        int n = rand()%100;
+        cout<<n<<endl;
+        int men[n][n], women[n][n];
+        
+        int posm[n][n], posw[n][n];
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                men[i][j] = j;
+                women[i][j] = j;
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            random_shuffle(men[i], men[i]+n, randf);        
+            for(int j = 0; j < n; j++) 
+            {
+                cout<<men[i][j]<<" ";  
+            }
+            cout<<endl;
+        }
+        for(int i = 0; i < n; i++) 
+        {
+            random_shuffle(women[i], women[i]+n, randf);
+            for(int j = 0; j < n; j++) 
+            {
+                cout<<women[i][j]<<" "; 
+            }
+            cout<<endl;
+        }
+    }
+    fclose(stdout);
     return 0;
 }
